784335bec02fac31c07c445edc00ce4228f58feb
frinkifail <pangpang01011998@gmail.com>
2022-12-16 10:29:28 +0700

    the ok :thumbsup:

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ gen1 = {
     "growth": 1.28,
     "amount": 0
 }
+buymax = False
 
 def main(page: ft.Page):
+    global buymax
+    # buymax = False
     def buygen1(e):
         global points, gen1, pointsperspec
         if points >= gen1["cost"]:
@@ -34,20 +37,40 @@ def main(page: ft.Page):
         contents = json.load(savefile)
         points = contents["points"]
         print("[save] loaded!")
-            
+    def handleDebugPts(e):
+        global points
+        points = int(debugpointstf.value)
+        print(f"[debug] set points to {0}".format(points))
+    def handleBuyMax(e):
+        global buymax
+        if buymax is False:
+            buymax = True
+            buymaxbtn.text = f"Buy Max: {buymax}"
+            page.update()
+        else:
+            buymax = False
+            buymaxbtn.text = f"Buy Max: {buymax}"
+            page.update()
     fpscounter = ft.Text("FPS: 0")
+    buymaxbtn = ft.TextButton(f"Buy Max: {buymax}", on_click=handleBuyMax)
+    page.add(buymaxbtn)
     pointscounter = ft.Text(str(points)+" points")
     buygen1button = ft.ElevatedButton(f"Buy generator 1 ({gen1['amount']}) | Cost: {gen1['cost']}$", on_click=buygen1)
     page.add(fpscounter, pointscounter, buygen1button)
     savefiletf = ft.TextField(label="Save ID", hint_text="Save ID")
+    debugpointstf = ft.TextField(label="Set Points")
     page.add(ft.Row([ft.ElevatedButton("Save", on_click=handleSave), ft.ElevatedButton("Load", on_click=handleLoad), savefiletf]))
+    page.add(debugpointstf, ft.IconButton(ft.icons.CHECK, on_click=handleDebugPts))
     while True:
+        # global buymax
         starttime = time.time()
         pointscounter.value = str(math.floor(points))+" points | "+str(pointsperspec)+" points per frame"
-        buygen1button.text = f"Buy generator 1 ({gen1['amount']}) | Cost: {gen1['cost']}$"
+        buygen1button.text = f"Buy Basic Generator ({gen1['amount']}) | Cost: {gen1['cost']}$"
         fpscounter.value = "FPS: " + str(1.0 / (time.time() - starttime)) # raw clock speed fps # due to reasons
         page.update()
         time.sleep(0.083333333333333328707) # make the game capped at 12 fps
+        if buymax is True:
+            buygen1(None)
 
 def update():
     while True:
